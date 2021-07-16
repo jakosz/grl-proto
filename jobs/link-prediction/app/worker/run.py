b004e9b9fb4797561b7374a9c0d387aab08ecbc9
Jacek Szejda <git@jacek.szejda.com>
2021-07-16 14:26:13 +0200

    add send_update to worker

--- jobs/link-prediction/app/worker/run.py
+++ jobs/link-prediction/app/worker/run.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 
+import argparse
 import base64
 import json
 import random
@@ -71,100 +72,110 @@ def get_training_sample(G, graph, max_nb, batch_size):
     return [x0, x1], y
 
 
-def send_results(x):
+def send_results(x, base_uri):
     b64 = base64.b64encode(json.dumps(x, cls=JsonNumpy, separators=(',',':')).encode()).decode()
+    req = urllib.request.urlopen(f"{base_uri}/{b64}")
     return b64 
+
 
 rg = lambda x: getattr(igraph.Graph, x)
 
 
-log = get_stdout_logger('link-prediction-worker')
-log.info('Starting job...')
-
-G, graph, sig = get_rgm(np.arange(config.VCOUNT_FROM, config.VCOUNT_TO), config.RGM_SAMPLING_SPACE)
-obs = int(sig['n'])
-nnb = [1, int(np.quantile(G.degree(), .9))]
-dims = config.DIM_RANGE 
-steps = config.TRAINING_STEPS
-batch_size = config.BATCH_SIZE
-
-log.info(f'Generated graph from {sig}')
-
-# eigen
-
-a = gtoa(G)
-val, vec = np.linalg.eig(a)
-val = np.real(val)
-vec = np.real(vec)
-
-for dim in dims:
-    try:
-
-        yhat = vec[:, :dim].dot(np.diag(val[:dim])).dot(vec.T[:dim, :])
-
-        res = {
-            'symmetric': True,
-            'diagonal': True,
-            'dim': int(dim),
-            'reducer': 'eig',
-            'max_nb': None,
-            'loss': tf.keras.losses.binary_crossentropy(a, yhat).numpy().mean(),
-            'acc': tf.keras.metrics.binary_accuracy(a, yhat).numpy().mean(),
-            'auc': roc_auc_score(a.ravel(), yhat.ravel()),
-            'rgm': sig,
-            'batch_size': None,
-            'steps': None
-        }
-
-        log.info(res)
-        log.info(f"Network request would look like this: {send_results(res)}") 
-
-    except Exception as e:
-        log.error(e)
-        sys.exit()
-
-# neuro
-
-for sym in [True, False]:
-    for diag in [True, False]:
-        for dim in dims:
-            
-            reducers = {
-                'mean': lambda x: tf.reduce_mean(x, axis=-2),
-                'gnn': grl.layers.GNNSimple(dim, n_layers=1, activation=None),
-                'gnn-gelu': grl.layers.GNNSimple(dim, n_layers=1, activation=tf.nn.gelu),
-                'gat-h1': grl.layers.GAT(dim, n_layers=1, n_heads=1),
-                'gat-h2': grl.layers.GAT(dim, n_layers=1, n_heads=2)
-            }
-            
-            for name, reducer in reducers.items():
-                for max_nb in nnb:
-                    model, latent = get_model(obs+1, dim, max_nb=max_nb, symmetric=sym, diagonal=diag, reducer=reducer)
-                    hist = []
-                    if max_nb == 1:
-                        for step in range(steps):
-                            x, y = get_nce_sample(G)
-                            x = [e+1 for e in x]
-                            hist.append(model.train_on_batch(x, y))
-                    else:
-                        for step in range(steps):
-                            x, y = get_training_sample(G, graph, max_nb, batch_size)
-                            hist.append(model.train_on_batch(x, y))
-                    hist = np.array(hist)
-                    
-                    res = {
-                        'symmetric': sym,
-                        'diagonal': diag,
-                        'dim': int(dim),
-                        'reducer': name,
-                        'max_nb': max_nb,
-                        'loss': hist[-100:, 0].mean(),
-                        'acc': hist[-100:, 1].mean(),
-                        'auc': hist[-100:, 2].mean(),
-                        'rgm': sig,
-                        'batch_size': batch_size,
-                        'steps': steps
-                    }
+if __name__ == '__main__':
     
-                    log.info(res)
-                    log.info(f"Network request would look like this: {send_results(res)}") 
+    p = argparse.ArgumentParser()
+    p.add_argument('-s', '--server')
+    args, _ = p.parse_known_args()
+
+    log = get_stdout_logger('link-prediction-worker')
+    log.info('Starting job...')
+
+    G, graph, sig = get_rgm(np.arange(config.VCOUNT_FROM, config.VCOUNT_TO), config.RGM_SAMPLING_SPACE)
+    obs = int(sig['n'])
+    nnb = [1, int(np.quantile(G.degree(), .9))]
+    dims = config.DIM_RANGE 
+    steps = config.TRAINING_STEPS
+    batch_size = config.BATCH_SIZE
+
+    log.info(f'Generated graph from {sig}')
+
+    # eigen
+
+    a = gtoa(G)
+    val, vec = np.linalg.eig(a)
+    val = np.real(val)
+    vec = np.real(vec)
+
+    for dim in dims:
+        try:
+
+            yhat = vec[:, :dim].dot(np.diag(val[:dim])).dot(vec.T[:dim, :])
+
+            res = {
+                'symmetric': True,
+                'diagonal': True,
+                'dim': int(dim),
+                'reducer': 'eig',
+                'max_nb': None,
+                'loss': tf.keras.losses.binary_crossentropy(a, yhat).numpy().mean(),
+                'acc': tf.keras.metrics.binary_accuracy(a, yhat).numpy().mean(),
+                'auc': roc_auc_score(a.ravel(), yhat.ravel()),
+                'rgm': sig,
+                'batch_size': None,
+                'steps': None
+            }
+
+            log.info(res)
+            send_results(res, args.server)
+            log.info(f"Sent results to {args.server}")
+
+        except Exception as e:
+            log.error(e)
+            sys.exit()
+
+    # neuro
+
+    for sym in [True, False]:
+        for diag in [True, False]:
+            for dim in dims:
+                
+                reducers = {
+                    'mean': lambda x: tf.reduce_mean(x, axis=-2),
+                    'gnn': grl.layers.GNNSimple(dim, n_layers=1, activation=None),
+                    'gnn-gelu': grl.layers.GNNSimple(dim, n_layers=1, activation=tf.nn.gelu),
+                    'gat-h1': grl.layers.GAT(dim, n_layers=1, n_heads=1),
+                    'gat-h2': grl.layers.GAT(dim, n_layers=1, n_heads=2)
+                }
+                
+                for name, reducer in reducers.items():
+                    for max_nb in nnb:
+                        model, latent = get_model(obs+1, dim, max_nb=max_nb, symmetric=sym, diagonal=diag, reducer=reducer)
+                        hist = []
+                        if max_nb == 1:
+                            for step in range(steps):
+                                x, y = get_nce_sample(G)
+                                x = [e+1 for e in x]
+                                hist.append(model.train_on_batch(x, y))
+                        else:
+                            for step in range(steps):
+                                x, y = get_training_sample(G, graph, max_nb, batch_size)
+                                hist.append(model.train_on_batch(x, y))
+                        hist = np.array(hist)
+                        
+                        res = {
+                            'symmetric': sym,
+                            'diagonal': diag,
+                            'dim': int(dim),
+                            'reducer': name,
+                            'max_nb': max_nb,
+                            'loss': hist[-100:, 0].mean(),
+                            'acc': hist[-100:, 1].mean(),
+                            'auc': hist[-100:, 2].mean(),
+                            'rgm': sig,
+                            'batch_size': batch_size,
+                            'steps': steps
+                        }
+        
+                        log.info(res)
+                        send_results(res, args.server)
+                        log.info(f"Sent results to {args.server}")
