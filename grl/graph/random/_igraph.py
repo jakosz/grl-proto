a854c42ce637577e7114b98a72833d9a2002e985
Jacek Szejda <git@jacek.szejda.com>
2021-09-25 17:17:17 +0200

    export all rgms in __init__

--- grl/graph/random/_igraph.py
+++ grl/graph/random/_igraph.py
@@ -1,37 +1,36 @@
-import random
+import random as _random
 
-import igraph
+import igraph as _igraph
 
-from ..utils import from_igraph
+from ..utils import _from_igraph as _from_igraph
 
 
 def barabasi(n, m, seed=None):
-    random.seed(seed)
-    g = igraph.Graph.Barabasi(n, m)
-    return from_igraph(g)
+    _random.seed(seed)
+    g = _igraph.Graph.Barabasi(n, m)
+    return _from_igraph(g)
 
 
 def block(n, pref_matrix, block_sizes, seed=None):
-    #random.seed(seed)
-    #g = igraph.Graph.SBM(n, pref_matrix, block_sizes)
-    #return from_igraph(g)
+    #_random.seed(seed)
+    #g = _igraph.Graph.SBM(n, pref_matrix, block_sizes)
+    #return _from_igraph(g)
     raise NotImplementedError("pref_matrix and block_sizes need conversions to/fro numpy")
 
 
 def erdos(n, p, seed=None):
-    random.seed(seed)
-    g = igraph.Graph.Erdos_Renyi(n, p)
-    return from_igraph(g)
+    _random.seed(seed)
+    g = _igraph.Graph.Erdos_Renyi(n, p)
+    return _from_igraph(g)
 
 
 def fire(n, fw, bw=0.0, ambs=1, seed=None):
-    random.seed(seed)
-    g = igraph.Graph.Forest_Fire(n, fw_prob=fw, bw_factor=bw, ambs=ambs)
-    return from_igraph(g)
+    _random.seed(seed)
+    g = _igraph.Graph.Forest_Fire(n, fw_prob=fw, bw_factor=bw, ambs=ambs)
+    return _from_igraph(g)
 
 
 def geometric(n, r, seed=None):
-    random.seed(seed)
-    g = igraph.Graph.GRG(n, r)
-    return from_igraph(g)
-
+    _random.seed(seed)
+    g = _igraph.Graph.GRG(n, r)
+    return _from_igraph(g)
