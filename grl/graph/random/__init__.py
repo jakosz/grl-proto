3273798a126cc7403d4e7b7fc7205b16874c160b
Jacek Szejda <jakosz@gmail.com>
2021-09-30 17:57:27 +0200

    add seed for numba jitted functions

--- grl/graph/random/__init__.py
+++ grl/graph/random/__init__.py
@@ -1,1 +1,12 @@
-from ._igraph import * 
+import numba as _numba
+import numpy as _np
+
+from ._igraph import *
+
+
+@_numba.njit()
+def seed(seed=None):
+    """ Set seed for numba jitted functions. 
+    """
+    if seed is not None:
+        _np.random.seed(seed)
