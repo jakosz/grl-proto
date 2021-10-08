6b15a7be2de301e8fd72ef920e3e9996f1a43f0c
Jacek Szejda <jakosz@gmail.com>
2021-10-08 14:04:38 +0200

    fix wrong argument name in LineCollection

--- grl/graph/plot.py
+++ grl/graph/plot.py
@@ -9,9 +9,9 @@ def layout_fruchterman_reingold(graph):
     return np.array(utils.to_igraph(graph).layout_fruchterman_reingold().coords)
 
 
-def plot(graph, xy=None):
+def plot(graph, xy=None, c='k', alpha=.8):
     if xy is None:
         xy = layout_fruchterman_reingold(graph)
     edgelist = utils.enumerate_edges(graph)-1 # @1-indexing
-    plt.scatter(xy[:, 0], xy[:, 1])
-    plt.gca().add_collection(LineCollection([xy[e].tolist() for e in edgelist]))
+    plt.scatter(xy[:, 0], xy[:, 1], c=c, s=10)
+    plt.gca().add_collection(LineCollection([xy[e].tolist() for e in edgelist], color=c, alpha=alpha))
