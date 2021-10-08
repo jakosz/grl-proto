f4e0b46e01ac43913d6f5873b0abf29c93a5fe56
Jacek Szejda <jakosz@gmail.com>
2021-10-08 14:19:45 +0200

    remove emoji from docstring - backslash was causing test warnings

--- grl/graph/mem/_obj.py
+++ grl/graph/mem/_obj.py
@@ -1,12 +1,10 @@
 """
 
-Empty namespace for shared memory objects. 
+Empty namespace for shared memory objects.
 
 If you want to use shared memory objects in Python functions
 they need to be present in the global namespace
 and cannot be passed around as function arguments.
-
-¯\_(ツ)_/¯
 
 """
 
