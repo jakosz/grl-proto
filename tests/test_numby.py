3ab4ed005f2bb44884428e48a937de4428eb6aac
Jacek Szejda <jakosz@gmail.com>
2021-10-09 11:52:55 +0200

    update tests for 0.4.12

---
+++ tests/test_numby.py
@@ -0,0 +1,15 @@
+import grl
+
+
+def test_clip():
+    pass
+
+
+def test_sigmoid():
+    assert grl.sigmoid(0.) == .5
+    assert grl.sigmoid(+1e+3) == 1
+    assert np.allclose([grl.sigmoid(-1e+2)], [0])
+
+
+def test_softmax():
+    pass
