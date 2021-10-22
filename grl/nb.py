4b90e4c0f498d7cb0d62b9a501f5315e410c9505
Jacek Szejda <jakosz@gmail.com>
2021-10-22 18:45:25 +0200

    add common Jupyter imports

---
+++ grl/nb.py
@@ -0,0 +1,11 @@
+""" Common imports for Jupyter notebook session.  
+"""
+
+import matplotlib.pyplot as plt
+import numba
+import numpy as np
+import ray
+import tensorflow as tf
+from tensorflow.keras.layers import *
+
+import grl
