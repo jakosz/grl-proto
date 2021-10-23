9b30bc5f87ac054e15b6627be138fa68f4544d38
Jacek Szejda <jakosz@gmail.com>
2021-10-23 02:30:14 +0200

    print imports from grl.nb

--- grl/nb.py
+++ grl/nb.py
@@ -10,3 +10,15 @@ from sklearn.metrics import roc_auc_score
 from tensorflow.keras.layers import *
 
 import grl
+
+
+inform_user = """import matplotlib.pyplot as plt
+import numba
+import numpy as np
+import ray
+import tensorflow as tf
+from sklearn.metrics import roc_auc_score
+from tensorflow.keras.layers import *
+
+import grl"""
+print(inform_user)
