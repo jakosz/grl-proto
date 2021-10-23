70c25e5868bc16bf06a4b1e65073e02c37bf37e3
Jacek Szejda <jakosz@gmail.com>
2021-10-23 07:08:47 +0200

    extend install script to pull from git and remount tmp in memory

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+git pull origin master --tag
+
 python3 -m pip install -r requirements.txt
 python3 -m pip install -e .
 python3 -m pytest -v
+
+sudo mount -o remount,size=100% /dev/shm     # increase the size of shared memory 
+sudo mount -o size=100% -t tmpfs tmpfs /tmp  # mount /tmp in RAM
