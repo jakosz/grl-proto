5a01742782c345ad575dbc86fecd7959b3032ee4
Jacek Szejda <jakosz@gmail.com>
2021-10-26 23:30:43 +0200

    add link-prediction job v4

---
+++ jobs/link-prediction/v4/run.py
@@ -0,0 +1,54 @@
+import argparse
+
+import json
+import time
+import traceback
+
+from grl.utils import JsonNumpy
+
+from src import *
+
+
+p = argparse.ArgumentParser()
+p.add_argument('--runs', type=int)
+p.add_argument('--dims', type=int, help='Upper limit of dimensions to scan')
+p.add_argument('--vcount', type=int)
+p.add_argument('--iter', type=int)
+p.add_argument('--output', type=str, default='link-prediction-v4.json')
+args, _ = p.parse_known_args()
+
+
+config.vcount = args.vcount
+config.dims = range(2, args.dims)
+
+
+cnt = 0
+times = 2.  # running average for printing info
+for run in range(args.runs):
+    for emb_name, emb_model in embs.items():
+        for rgm_name, rgm_model in rgms.items():
+            for dim in dims:
+                try:
+                    g = rgm_model()
+                    t0 = time.time()
+                    auc = emb_model(g, dim)
+                    t1 = time.time()
+
+                    res = {
+                        'auc': auc, 
+                        'dim': dim,
+                        'eigen': eigen(g, dim),
+                        'emb': emb_name,
+                        'properties': properties(g),
+                        'rgm': rgm_name
+                    }
+
+                    with open(args.output, 'a') as f:
+                        f.write(json.dumps(res, cls=JsonNumpy) + "\n")
+
+                    cnt += 1
+                    times = times*.99 + (t1-t0)*.01
+                    print(f"{cnt:,} models total - {times:.04f} sec./run    ", end="\r", flush=True)
+                except:
+                    with open('exceptions.txt', 'a') as f:
+                        f.write(traceback.format_exc() + f"\n\n\n{'-'*80}\n\n\n")
---
+++ jobs/link-prediction/v4/src.py
@@ -0,0 +1,94 @@
+import numpy as np
+from scipy.stats import entropy
+from sklearn.metrics import roc_auc_score
+
+import grl
+
+
+class Namespace:
+    pass
+
+
+def properties(g):
+    a = grl.graph.utils.to_adjacency(g)
+    g = grl.graph.utils.to_igraph(g)
+    return {
+        'assortativity_degree': g.assortativity_degree(),
+        'average_path_length': g.average_path_length(),
+        'clique_number': g.clique_number(),
+        'diameter': g.diameter(),
+        'density': g.density(),
+        'edge_connectivity': g.edge_connectivity(),
+        'entropy': scipy.stats.entropy(a.ravel(), base=2),
+        'girth': g.girth(),
+        'independence_number': g.independence_number(),
+        'is_bipartite': g.is_bipartite(),
+        'is_chordal': g.is_chordal(),
+        'is_connected': g.is_connected(),
+        'maxdegree': g.maxdegree(),
+        'motifs_randesu_3': np.array(g.motifs_randesu(3)),
+        'motifs_randesu_4': np.array(g.motifs_randesu(4)),
+        'transitivity_undirected': g.transitivity_undirected(),
+        'vertex_connectivity': g.vertex_connectivity()
+    }
+
+
+def barabasi():
+    return grl.graph.random.barabasi(config.vcount, config.range_barabasi())
+
+
+def erdos():
+    return grl.graph.random.erdos(config.vcount, config.range_erdos())
+
+
+def geometric():
+    return grl.graph.random.geometric(config.vcount, config.range_geometric())
+
+
+def eigen(graph, dim):
+    a = grl.graph.utils.to_adjacency(graph)
+    l, v = grl.graph.embed.eigen.encode(graph)
+    y = grl.graph.embed.eigen.decode(l, v, dim)
+    return roc_auc_score(a.ravel(), y.ravel())
+
+
+def diagonal(graph, dim):
+    a = grl.graph.utils.to_adjacency(graph)
+    x, d = grl.graph.embed.diagonal.encode(graph, dim, config.lr, config.iter)
+    y = grl.graph.embed.diagonal.decode(x, d)
+    return roc_auc_score(a.ravel(), y.ravel())
+
+
+def symmetric(graph, dim):
+    a = grl.graph.utils.to_adjacency(graph)
+    x = grl.graph.embed.symmetric.encode(graph, dim, config.lr, config.iter)
+    y = grl.graph.embed.symmetric.decode(x)
+    return roc_auc_score(a.ravel(), y.ravel())
+
+
+def asymmetric(graph, dim):
+    a = grl.graph.utils.to_adjacency(graph)
+    l, r = grl.graph.embed.asymmetric.encode(graph, dim, config.lr, config.iter)
+    y = grl.graph.embed.asymmetric.decode(l, r)
+    return roc_auc_score(a.ravel(), y.ravel())
+
+
+embs = {
+    'diagonal': diagonal, 
+    'symmetric': symmetric, 
+    'asymmetric': asymmetric
+}
+
+rgms = {
+    'barabasi': barabasi, 
+    'erdos': erdos, 
+    'geometric': geometric
+}
+
+config = Namespace()
+config.vcount = None  # passed from the caller
+config.iter = 2**21 
+config.lr = .025
+config.range_barabasi = lambda: np.random.choice(np.arange(2, 41))
+config.range_erdos = lambda: np.random.uniform()
+config.range_geometric = lambda: np.random.uniform()
