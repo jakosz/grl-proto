0be9a51d811162c38d55ed7e0a05556ed15bb46a
Jacek Szejda <jakosz@gmail.com>
2021-10-28 17:08:56 +0200

    add shared batch size calculation for embedding functions

---
+++ grl/graph/embed/_embed.py
@@ -0,0 +1,12 @@
+import numba
+
+
+@numba.njit()
+def _embed_batch_size(steps, cores):
+    # Calculate the number of iterations to be performed on each thread. 
+    # This number needs to be even because samplers return n//2 positives 
+    # and n//2 negatives to keep the batches balanced.  
+    # This leads to segfaults when steps//cores happen to be an odd number, 
+    # so we need to correct for that. 
+    res = steps//cores
+    return res - res % 2
--- grl/graph/embed/asymmetric.py
+++ grl/graph/embed/asymmetric.py
@@ -2,20 +2,21 @@ import numba
 import numpy as np
 
 import grl
-
+from _embed import *
 
 @numba.njit(fastmath=True, parallel=True)
 def encode(graph, dim, lr, steps):
     
     cores = numba.config.NUMBA_NUM_THREADS
+    n = _embed_batch_size(steps, cores)
     L = np.random.randn(grl.vcount(graph)+1, dim)/dim  # @indexing
     R = np.random.randn(grl.vcount(graph)+1, dim)/dim  # @indexing
 
     for i in numba.prange(cores):
         
-        x, y = grl.graph.sample.neg(graph, steps//cores)
+        x, y = grl.graph.sample.neg(graph, n)
         
-        for j in range(steps//cores):
+        for j in range(n):
 
             xL = L[x[j, 0]]
             xR = R[x[j, 1]]
@@ -27,7 +28,7 @@ def encode(graph, dim, lr, steps):
             grl.clip_1d_inplace(dxL, -grl.CLIP, grl.CLIP)
             grl.clip_1d_inplace(dxR, -grl.CLIP, grl.CLIP)
 
-            cos = grl.cos_decay(j / (steps//cores))
+            cos = grl.cos_decay(j/n) 
             L[x[j, 0]] -= dxL*lr*cos
             R[x[j, 1]] -= dxR*lr*cos
             
