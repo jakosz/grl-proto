758e0411d86fc8491f766ca81a889a10b49d01d8
Jacek Szejda <jakosz@gmail.com>
2021-10-29 14:02:16 +0200

    further expand explanation on how to do Python shmem hacking

--- grl/graph/embed/__init__.py
+++ grl/graph/embed/__init__.py
@@ -6,13 +6,19 @@ computation without memory locks needs to be implemented in Python:
     * numba function doing the computation must take it as an argument, but also must modify it in place
 
 These considerations are implemented as follows: 
-    1. `worker` implements the model fitting computation we want to parallelise. 
+    1. `worker` implements the model fitting computation that we want to parallelise. 
     2. `worker_mp_wrapper` takes the names of the parameter arrays,  
         translates them to pointers and calls `worker`.  
         This way we can pass RawArrays around without actually passing them around. 
     3. `encode` creates arrays and phony names as members of the shmem._obj, 
-        so they are accessible from the global namespace before ProcessPoolExecutor is created.  
+        so they are accessible from the global namespace before ProcessPoolExecutor is created. 
 
+Oh, by the way, for all this to even work you have to mount /tmp in memory before starting the Python program:
+    
+    $ sudo mount -o remount,size=100% /dev/shm     # increase the size of shared memory
+    $ sudo mount -o size=100% -t tmpfs tmpfs /tmp  # mount /tmp in RAM
+
+From now on it's smooth sailing xD
 """
 
 from . import asymmetric 
