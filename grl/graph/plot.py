5bd2a8361b3ccf50997362d45d269733e56b0c0b
Jacek Szejda <jakosz@gmail.com>
2021-12-16 11:40:50 +0100

    expose s as argument to graph.plot

--- grl/graph/plot.py
+++ grl/graph/plot.py
@@ -9,9 +9,9 @@ def layout_fruchterman_reingold(graph):
     return np.array(utils.to_igraph(graph).layout_fruchterman_reingold().coords)
 
 
-def plot(graph, xy=None, c='k', alpha=.8):
+def plot(graph, xy=None, c='k', s=10, alpha=.8):
     if xy is None:
         xy = layout_fruchterman_reingold(graph)
     edgelist = utils.enumerate_edges(graph)-1  # @indexing
-    plt.scatter(xy[:, 0], xy[:, 1], c=c, s=10)
+    plt.scatter(xy[:, 0], xy[:, 1], c=c, s=s)
     plt.gca().add_collection(LineCollection([xy[e].tolist() for e in edgelist], color=c, alpha=alpha))
