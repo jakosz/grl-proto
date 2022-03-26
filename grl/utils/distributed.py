58890f54e2be589de717ac4db4e1130bf7d2de96
Jacek Szejda <jakosz@gmail.com>
2022-03-26 01:24:38 +0100

    expose tick

--- grl/utils/distributed.py
+++ grl/utils/distributed.py
@@ -5,14 +5,14 @@ from . import ipc
 
 
 class MirroredArray:
-    def __init__(self, arr, hosts, port, sample_size=1):
+    def __init__(self, arr, hosts, port, sample_size=1, tick=0):
         self.arr = arr
         self.hosts = hosts
         self.port = port
-        self.sender = sender(self.arr, sample_size, port).start()
+        self.sender = sender(self.arr, sample_size, port, tick=tick).start()
         self.receivers = []
         for host in hosts:
-            self.receivers.append(receiver(self.arr, host, port).start())
+            self.receivers.append(receiver(self.arr, host, port, tick=tick).start())
     
     def __getattr__(self, *args, **kwargs):
         return getattr(self.arr, *args, **kwargs)
@@ -27,9 +27,9 @@ class MirroredArray:
         self.arr[i] = v
 
 
-def receiver(arr, host, port=5555):
+def receiver(arr, host, port=5555, tick=0):
     ctx, sock = ipc.socket_pull(host, port)
-    @utils.background(tick=0)
+    @utils.background(tick=tick)
     @ipc.pull(sock)
     def _receiver(data, arr):
         t0 = arr[data['key']].copy()
@@ -38,9 +38,9 @@ def receiver(arr, host, port=5555):
     return _receiver(arr)
 
 
-def sender(arr, sample_size, port=5555):
+def sender(arr, sample_size, port=5555, tick=0):
     ctx, sock = ipc.socket_push(port)
-    @utils.background(tick=0)
+    @utils.background(tick=tick)
     @ipc.push(sock)
     def _sender(arr, sample_size):
         i = np.random.choice(arr.shape[0], sample_size)
