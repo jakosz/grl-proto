d6782639005c8b2af569e8148e77b3134bbae412
Jacek Szejda <jakosz@gmail.com>
2022-04-04 21:56:42 +0200

    update docstring

--- grl/__init__.py
+++ grl/__init__.py
@@ -1,3 +1,9 @@
+""" This is grl.
+
+Some general notes: 
+* I try to mark places where conversions between 1- and 0- indexing are taking place with a `@indexing` comment.
+* Many tests are carried out against `igraph`, `numpy` and `tensorflow`. This is not very elegant, and shouldn't be *the only* case, but for initial development should suffice. 
+"""
 from . import graph
 from . import metrics
 from . import utils
