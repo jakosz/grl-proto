399da626687dad0f583d106bd0a7e88213dbee91
Jacek Szejda <jakosz@gmail.com>
2022-04-04 20:46:48 +0200

    add module-level model imports

--- grl/graph/__init__.py
+++ grl/graph/__init__.py
@@ -2,10 +2,12 @@ from . import core
 from . import decode
 from . import embed
 from . import encode
+from . import model
 from . import neighbours
 from . import random
 from . import sample
 from . import utils
 
+from .model import Model
 from .plot import plot
 from .utils import load, save
