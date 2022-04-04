8397beb7264de3fdc18196512748a8dbc1a89f5b
Jacek Szejda <jakosz@gmail.com>
2022-04-04 19:07:56 +0200

    add stub of the new model api

---
+++ grl/graph/model/utils.py
@@ -0,0 +1,12 @@
+import numba
+
+
+@numba.njit(cache=True)
+def split_steps(steps, cores):
+    # Calculate the number of iterations to be performed on each thread. 
+    # This number needs to be even because samplers return n//2 positives 
+    # and n//2 negatives to keep the batches balanced.  
+    # This leads to segfaults when steps//cores happen to be an odd number, 
+    # so we need to correct for that. 
+    res = steps//cores
+    return res - res % 2
