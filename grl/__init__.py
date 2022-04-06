b9b97c79484ba5f6fbe5d88d1f5bba0dccb9d1b3
Jacek Szejda <jakosz@gmail.com>
2022-04-07 00:21:09 +0200

    Suppress tf loggin verbosity.

--- grl/__init__.py
+++ grl/__init__.py
@@ -4,6 +4,9 @@ Some general notes:
 * I try to mark places where conversions between 1- and 0- indexing are taking place with a `@indexing` comment.
 * Many tests are carried out against `igraph`, `numpy` and `tensorflow`. This is not very elegant, and shouldn't be *the only* case, but for initial development should suffice. 
 """
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 from . import graph
 from . import metrics
 from . import utils
@@ -12,4 +15,4 @@ from .nn import *
 from .numby import *
 from .shmem import *
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
