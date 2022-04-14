c9cfe6fade4fdde0462c0260a3aeb0a1220d97c9
Jacek Szejda <jakosz@gmail.com>
2022-04-14 22:05:39 +0200

    Bump version: 0.8.9 → 0.8.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Jacek Szejda'
 author = 'Jacek Szejda'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.9'
+release = '0.8.10'
 
 
 # -- General configuration ---------------------------------------------------
