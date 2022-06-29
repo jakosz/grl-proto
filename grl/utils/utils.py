ac798a25a1aa684da45e15bcfebc491953f1bdd5
Jacek Szejda <jakosz@gmail.com>
2022-06-29 14:01:24 +0200

    datagen: return self on __iter__

--- grl/utils/utils.py
+++ grl/utils/utils.py
@@ -34,7 +34,7 @@ class DataGen:
         self.stop()
         
     def __iter__(self):
-        return self.__next__()
+        return self
             
     def __next__(self):
         if not self._stop:
