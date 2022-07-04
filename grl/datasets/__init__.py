1bb7c937384f3b37465417d4dd51ff0ee2a4781b
Jacek Szejda <jakosz@gmail.com>
2022-07-04 15:59:51 +0200

    fix absolute path construction

--- grl/datasets/__init__.py
+++ grl/datasets/__init__.py
@@ -6,12 +6,12 @@ HERE = os.path.dirname(__file__)
 
 
 def citeseer():
-    graph = utils.load('data/citeseer/citeseer.grl')
-    targets = np.load('data/citeseer/citeseer-targets.npy')
+    graph = utils.load(f'{HERE}/data/citeseer/citeseer.grl')
+    targets = np.load(f'{HERE}/data/citeseer/citeseer-targets.npy')
     return graph, targets
 
 
 def cora():
-    graph = utils.load('data/cora/cora.grl')
-    targets = np.load('data/cora/cora-targets.npy')
+    graph = utils.load(f'{HERE}/data/cora/cora.grl')
+    targets = np.load(f'{HERE}/data/cora/cora-targets.npy')
     return graph, targets
