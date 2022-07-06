cbde6881f1ce896a9297ebbf7a28b895b0b659b9
Jacek Szejda <jakosz@gmail.com>
2022-07-06 15:35:48 +0200

    add dropout to symmetric & diagonal models

--- grl/graph/model/workers.py
+++ grl/graph/model/workers.py
@@ -8,7 +8,10 @@ from grl import numby
 @numba.njit(cache=True)
 def asymmetric(x, y, L, R, lr, activation, dropout):
     n = x.shape[0]
-    j = np.random.choice(L.shape[1], int(L.shape[1]*(1-dropout)))
+    if dropout > 0:
+        j = np.random.choice(L.shape[1], int(L.shape[1]*(1-dropout)))
+    else:
+        j = np.arange(L.shape[1])
     for i in range(n):
         xL = L[x[i, 0]][j]
         xR = R[x[i, 1]][j]
@@ -30,9 +33,13 @@ def clip(x):
 @numba.njit(cache=True)
 def diagonal(x, y, L, D, lr, activation):
     n = x.shape[0]
+    if dropout > 0:
+        j = np.random.choice(L.shape[1], int(L.shape[1]*(1-dropout)))
+    else:
+        j = np.arange(L.shape[1])
     for i in range(n):
-        xL = L[x[i, 0]]
-        xR = L[x[i, 1]]
+        xL = L[x[i, 0]][j]
+        xR = L[x[i, 1]][j]
         # compute gradients
         dy = activation(np.sum(xL*xR*D)) - y[i]  # output
         dxLR = D*dy  # embedding product 
@@ -40,21 +47,25 @@ def diagonal(x, y, L, D, lr, activation):
         dxL = clip(xR*dxLR)  # left vector
         dxR = clip(xL*dxLR)  # right vector
         # update parameters
-        L[x[i, 0]] -= dxL*lr
-        L[x[i, 1]] -= dxR*lr
-        D[:] -= dD*lr
+        L[x[i, 0]][j] -= dxL*lr
+        L[x[i, 1]][j] -= dxR*lr
+        D[j] -= dD*lr
 
 
 @numba.njit(cache=True)
 def symmetric(x, y, L, lr, activation):
     n = x.shape[0]    
+    if dropout > 0:
+        j = np.random.choice(L.shape[1], int(L.shape[1]*(1-dropout)))
+    else:
+        j = np.arange(L.shape[1])
     for i in range(n):
-        xL = L[x[i, 0]]
-        xR = L[x[i, 1]]
+        xL = L[x[i, 0]][j]
+        xR = L[x[i, 1]][j]
         # compute gradients
         dy = activation(np.sum(xL*xR)) - y[i]
         dxL = clip(xR*dy)
         dxR = clip(xL*dy)
         # update parameters
-        L[x[i, 0]] -= dxL*lr
-        L[x[i, 1]] -= dxR*lr
+        L[x[i, 0]][j] -= dxL*lr
+        L[x[i, 1]][j] -= dxR*lr
