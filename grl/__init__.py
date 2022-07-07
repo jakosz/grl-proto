bc46854d2450c18d9153b69bd6ec51e6421a0a9f
Jacek Szejda <jakosz@gmail.com>
2022-07-07 19:23:28 +0200

    Bump version: 0.8.41 → 0.8.42

--- grl/__init__.py
+++ grl/__init__.py
@@ -16,4 +16,4 @@ from .nn import *
 from .numby import *
 from .shmem._ops import *
 
-__version__ = "0.8.41"
+__version__ = "0.8.42"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='grl',
-      version='0.8.41',
+      version='0.8.42',
       description='Graph Representation Learning',
       author='Jacek Szejda')
